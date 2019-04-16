d34fbb08dfdf3e5a62cfa2eb0275e17e7bafb281
Chris <christian.d.basso@gmail.com>
2019-04-16 00:14:42 -0300

    Login en español

--- src/main/java/com/tp/proyecto1/controllers/LoginController.java
+++ src/main/java/com/tp/proyecto1/controllers/LoginController.java
@@ -4,6 +4,7 @@ import com.tp.proyecto1.model.users.User;
 import com.tp.proyecto1.services.UserService;
 import com.tp.proyecto1.views.LoginView;
 import com.vaadin.flow.component.login.AbstractLogin;
+import com.vaadin.flow.component.login.LoginI18n;
 import com.vaadin.flow.spring.annotation.UIScope;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -38,7 +39,25 @@ public class LoginController{
 				loginView.getLoginComponent().setError(true);
 			}
 		});
+		loginView.getLoginComponent().setI18n(createSpanishI18n());
 
+	}
+
+	private LoginI18n createSpanishI18n() {
+		final LoginI18n i18n = LoginI18n.createDefault();
+
+		i18n.setHeader(new LoginI18n.Header());
+		i18n.getHeader().setTitle("Al Planeta");
+		i18n.getHeader().setDescription("Reserva de vuelos");
+		i18n.getForm().setUsername("Usuario");
+		i18n.getForm().setTitle("Acceso a su cuenta");
+		i18n.getForm().setSubmit("Entrar");
+		i18n.getForm().setPassword("Contraseña");
+		i18n.getForm().setForgotPassword("¿olvidó su contraseña?");
+		i18n.getErrorMessage().setTitle("Usuario/Contraseña inválidos");
+		i18n.getErrorMessage().setMessage("Confirme su usuario y contraseña e intentelo nuevamente.");
+
+		return i18n;
 	}
 
 	private boolean authenticate(AbstractLogin.LoginEvent e) {
