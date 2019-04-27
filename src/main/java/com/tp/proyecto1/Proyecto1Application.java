a553a30983c1610421b8ff8ce63cdb22b5b96e51
chrisbasso <45736051>
2019-04-27 09:34:36 -0300

    Cambio Conexion a MySQL

--- src/main/java/com/tp/proyecto1/Proyecto1Application.java
+++ src/main/java/com/tp/proyecto1/Proyecto1Application.java
@@ -9,6 +9,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+
 @SpringBootApplication
 public class Proyecto1Application {
 
