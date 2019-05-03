d74d79b0f544cbd9e30ecce9c13a47ef56e598f5
Derek <drkmai@gmail.com>
2019-05-02 23:46:52 -0300

    Promocion, PromocionRepository & PromocionService

---
+++ src/main/java/com/tp/proyecto1/repository/viajes/PromocionRepository.java
@@ -0,0 +1,11 @@
+package com.tp.proyecto1.repository.viajes;
+
+import com.tp.proyecto1.model.viajes.Promocion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PromocionRepository extends JpaRepository<Promocion, Long> {
+
+
+}
