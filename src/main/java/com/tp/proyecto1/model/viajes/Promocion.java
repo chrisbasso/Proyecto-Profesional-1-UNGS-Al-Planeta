a7d3a00144cc0ad3c2ac1c630a16019c13a19b20
Derek Mai <drkmai@gmail.com>
2019-05-11 13:22:29 -0300

    pasado a tabla por clase

--- src/main/java/com/tp/proyecto1/model/viajes/Promocion.java
+++ src/main/java/com/tp/proyecto1/model/viajes/Promocion.java
@@ -9,7 +9,7 @@ import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 
 @Entity
-@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
+@Inheritance(strategy=InheritanceType.TABLE_PER_CLASS)
 public abstract class Promocion
 {
 	@Id
