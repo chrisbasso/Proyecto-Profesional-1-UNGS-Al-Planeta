ca60976627a2dd4feb5ba521d19ec8f8550aa944
Derek <drkmai@gmail.com>
2019-05-17 21:52:37 -0300

    metodos para buscar los las promociones de un viaje, un destino y de los tags

--- src/main/java/com/tp/proyecto1/repository/viajes/PromocionRepository.java
+++ src/main/java/com/tp/proyecto1/repository/viajes/PromocionRepository.java
@@ -1,12 +1,23 @@
 package com.tp.proyecto1.repository.viajes;
 
+import java.util.Set;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.tp.proyecto1.model.viajes.Destino;
 import com.tp.proyecto1.model.viajes.Promocion;
+import com.tp.proyecto1.model.viajes.TagDestino;
+import com.tp.proyecto1.model.viajes.Viaje;
 //import com.tp.proyecto1.model.viajes.PromocionPuntos;
 
 @Repository
 public interface PromocionRepository extends JpaRepository<Promocion, Long>{
 
+	
+	Set<Promocion> findByViajesAfectados(Viaje viaje);
+	
+	Set<Promocion> findByDestinosAfectados(Destino destino);
+	
+	Set<Promocion> findByTagsDestinoAfectados(TagDestino tag);
 }
