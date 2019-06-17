726dfce951b3cc6aa147ef9822bd3c85a7c99338
Derek <drkmai@gmail.com>
2019-06-17 17:33:34 -0300

    Preparando eventos para separarlos por empleado y por Local.

--- src/main/java/com/tp/proyecto1/repository/eventos/EventoRepository.java
+++ src/main/java/com/tp/proyecto1/repository/eventos/EventoRepository.java
@@ -2,6 +2,8 @@ package com.tp.proyecto1.repository.eventos;
 
 import com.tp.proyecto1.model.clientes.Persona;
 import com.tp.proyecto1.model.eventos.Evento;
+import com.tp.proyecto1.model.users.User;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +13,7 @@ import java.util.List;
 public interface EventoRepository extends JpaRepository<Evento, Long> {
 
 	List<Evento> findAllByPersona(Persona persona);
+	
+	List<Evento> findAllByCreadorEvento(User creador);
+	
 }
